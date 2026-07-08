3904ddcdc6a7d03a82bc98f2d13bf91e856dd607
Ayoub <ayoubdhamer5@gmail.com>
2026-07-08 23:13:23 +0100

    implementing light and dark mode css changes

--- src/app/components/unauthorized/unauthorized.component.ts
+++ src/app/components/unauthorized/unauthorized.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-unauthorized',
   templateUrl: './unauthorized.component.html',
-  styleUrls: ['./unauthorized.component.css']
+  styleUrls: ['./unauthorized.component.css'],
 })
 export class UnauthorizedComponent {
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+  ) {}
 
-  constructor(private location: Location) {}
-
-  goBack() {
-    this.location.back();
+  goHome(): void {
+    this.auth.getRoles().subscribe((roles) => {
+      if (roles.includes('ROLE_ADMIN')) {
+        this.router.navigate(['/admin']);
+      } else if (roles.includes('ROLE_USER')) {
+        this.router.navigate(['/user']);
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
   }
-
 }
